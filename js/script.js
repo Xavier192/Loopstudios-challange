2fc1d4d2a5139c5df355150f9a5326885243aa75
Xavier <xavier01242@gmail.com>
2021-10-01 05:32:33 +0200

    Compressed images and mobile version done

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-const navbar = document.querySelector('.header');
+const navbar = document.querySelector('.nav');
 const logo = document.querySelector('.nav__logo img');
+const navMenu = document.querySelector('.nav__menu');
+const navToggler = document.querySelector('.nav__toggler');
+const togglerImg = document.querySelector('.nav__toggler img');
+let navbarMenuState = false;
+let stopAnimationsTimer = null;
+
+navToggler.onclick = function(){
+    navMenu.classList.toggle('nav__toggle');
+
+    if(navbarMenuState){
+        navbarMenuState = false;
+        togglerImg.src = 'img/icon-hamburger.svg';
+    }
+    else{
+        navbarMenuState = true;
+        togglerImg.src = 'img/icon-close.svg';
+    }
+}
+
+window.onresize = function(){
+    document.body.id = 'stop-animations';
+    stopAnimationsTimer = setTimeout(function(){document.body.id = '';},400);
+}
 
 window.onscroll = function(){
     if (window.pageYOffset > 0) {
         navbar.classList.add('nav__scroll');
-        logo.src = 'img/logo-scroll.svg';
     }
     else {
         navbar.classList.remove('nav__scroll');
-        logo.src = 'img/logo.svg';
     }
 }
